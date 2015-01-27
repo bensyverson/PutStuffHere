fbd52b3e9a40c925b33f8863a9170bacc2054908
Ben Syverson <ben@bensyverson.com>
2015-01-26 19:09:32 -0600

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-connect = require('connect');
-connect.createServer(
-  connect.compiler({ src: __dirname + ".", enable: ['less'] }),
-  connect.staticProvider(__dirname + ".")
-).listen(3000);
-
-livereload = require('livereload');
-server = livereload.createServer({exts: ['less']});
-server.watch(__dirname + ".");
